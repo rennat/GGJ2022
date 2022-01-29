f2d1af6652ff424a7f66a686882e7f17e2f7e754
jkeogh1413 <jkeogh1413@gmail.com>
2022-01-29 13:35:04 -0600

    NavMesh v1

--- GGJ2022/Assets/NPCManager.cs
+++ GGJ2022/Assets/NPCManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 public class NPCManager : MonoBehaviour, IPointerClickHandler
@@ -15,14 +16,20 @@ public class NPCManager : MonoBehaviour, IPointerClickHandler
     float curCoreDamage;
 
     bool damagingCore = false;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start() {
         curHealth = totalHealth;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
         if (mode == "wander")
             StartCoroutine(WanderCoroutine()); 
         else if (mode == "rush")
-            StartCoroutine(MoveCoroutine()); 
+            StartCoroutine(MoveCoroutine());
     }
 
     IEnumerator MoveCoroutine() {
@@ -30,9 +37,10 @@ public class NPCManager : MonoBehaviour, IPointerClickHandler
 
         // Pick a target point
         Vector2 target = Vector2.zero;
+        agent.SetDestination(target);
         
         while (Vector2.Distance(transform.position, target) > 0.1f) {
-            transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime * speed);
+            //transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime * speed);
             yield return null;
         }
         yield return null;
@@ -56,10 +64,14 @@ public class NPCManager : MonoBehaviour, IPointerClickHandler
         curCoreDamage = 0f;
 
         // Pick a target point
-        Vector2 target = UnityEngine.Random.insideUnitCircle.normalized * 20f;
-        
-        while (Vector2.Distance(transform.position, target) > 0.1f) {
-            transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime * 1f);
+        Vector2 target = UnityEngine.Random.insideUnitCircle.normalized * 9f;
+        agent.SetDestination(target);
+
+        float deathTimout = 10f;
+        float curFleeTime = 0f;
+        while (Vector2.Distance(transform.position, target) > 0.1f && curFleeTime < deathTimout) {
+            curFleeTime += Time.deltaTime;
+            //transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime * 1f);
             yield return null;
         }
         Die();
@@ -75,6 +87,10 @@ public class NPCManager : MonoBehaviour, IPointerClickHandler
     }
 
     public void Disarm() {
+        if (mode == "flee")
+            return;
+        mode = "flee";
+
         StopAllCoroutines();
         StartCoroutine(FleeCoroutine());
     }
@@ -87,9 +103,11 @@ public class NPCManager : MonoBehaviour, IPointerClickHandler
     }
 
     public void Die() {
+        agent.isStopped = true;
         damagingCore = false;
         StopAllCoroutines();
-        Destroy(this);
+
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
