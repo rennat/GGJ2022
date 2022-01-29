c7d12f1d90bb852362b938b58ad78bda55a48b5b
jkeogh1413 <jkeogh1413@gmail.com>
2022-01-28 21:00:26 -0600

    PhaseManager WIP

---
+++ GGJ2022/Assets/PhaseManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseManager : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
