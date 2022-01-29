8b9d5ce693e670e3c97a476418be9b572e67b545
JBrooksCS <jasonbrooksbio@gmail.com>
2022-01-29 14:16:46 -0600

    boilerplate character movement for getting started with Unity

---
+++ GGJ2022/Assets/PlayerMovementJason.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovementJason : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+
+    void Update(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
