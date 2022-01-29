effc3e95a96a17e0597030c6d1d2a9c8fb373f67
jkeogh1413 <jkeogh1413@gmail.com>
2022-01-29 00:17:30 -0600

    Core health display

--- GGJ2022/Assets/CoreManager.cs
+++ GGJ2022/Assets/CoreManager.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoreManager : MonoBehaviour
 {
     public float totalHealth = 500f;
+    public TMP_Text healthText;
 
     float curHealth;
 
     private void Start() {
         curHealth = totalHealth;
+        healthText.text = curHealth.ToString();
     }
 
     public void TakeDamage(float damage) {
         curHealth -= damage;
-        Debug.LogError(curHealth);
+        healthText.text = curHealth.ToString();
 
         if (curHealth <= 0f) {
             Die();
