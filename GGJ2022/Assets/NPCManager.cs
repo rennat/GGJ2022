5551305dcce8bd0543a287c702c0da75190b4073
jkeogh1413 <jkeogh1413@gmail.com>
2022-01-29 00:42:47 -0600

    Flee when clicked aka given an umbrella

--- GGJ2022/Assets/NPCManager.cs
+++ GGJ2022/Assets/NPCManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NPCManager : MonoBehaviour
+public class NPCManager : MonoBehaviour, IPointerClickHandler
 {
+    public float totalHealth = 100f;
     public float speed = 0.1f;
     public float coreDamage = 1f;
     public string mode = "wander";
     public float wanderRadius = 0.5f;
+    float curHealth;
+    float curCoreDamage;
 
     // Start is called before the first frame update
     void Start() {
+        curHealth = totalHealth;
         if (mode == "wander")
             StartCoroutine(WanderCoroutine()); 
         else if (mode == "rush")
@@ -18,6 +23,8 @@ public class NPCManager : MonoBehaviour
     }
 
     IEnumerator MoveCoroutine() {
+        curCoreDamage = coreDamage;
+
         // Pick a target point
         Vector2 target = Vector2.zero;
         
@@ -30,6 +37,8 @@ public class NPCManager : MonoBehaviour
 
     IEnumerator WanderCoroutine() {
         while (mode == "wander") {
+            curCoreDamage = 0f;
+
             Vector2 newTarget = transform.position + new Vector3(Random.Range(-wanderRadius, wanderRadius), Random.Range(-wanderRadius, wanderRadius));
 
             while (Vector2.Distance(transform.position, newTarget) > 0.1f) {
@@ -40,12 +49,30 @@ public class NPCManager : MonoBehaviour
         }
     }
 
+    IEnumerator FleeCoroutine() {
+        curCoreDamage = 0f;
+
+        // Pick a target point
+        Vector2 target = UnityEngine.Random.insideUnitCircle.normalized * 20f;
+        
+        while (Vector2.Distance(transform.position, target) > 0.1f) {
+            transform.position = Vector2.Lerp(transform.position, target, Time.deltaTime * 1f);
+            yield return null;
+        }
+        yield return null;
+    }
+
     public void convert() {
         if (mode == "wander") {
             StopAllCoroutines();
             mode = "rush";
             StartCoroutine(MoveCoroutine());
         }
+    }
+
+    public void disarm() {
+        StopAllCoroutines();
+        StartCoroutine(FleeCoroutine());
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -58,7 +85,11 @@ public class NPCManager : MonoBehaviour
 
     void handleCollision(Collider2D col) {
         if (col.tag == "Core") {
-            col.GetComponent<CoreManager>().TakeDamage(coreDamage);
+            col.GetComponent<CoreManager>().TakeDamage(curCoreDamage);
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        disarm();
     }
 }
